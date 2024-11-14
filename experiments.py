5012ae2e5919e8bb2024eec0e8f0110e6a820814
hippolytepilchen <hippolytepilchen@slogin-02.cm.cluster>
2024-11-14 17:46:20 +0100

    exp pipeline

--- experiments.py
+++ experiments.py
@@ -1,23 +1,18 @@
 " This file create a config dictionary to launch training with the specified hyperparameters. "
 import argparse
 from hashlib import sha1
-from train import train
 import yaml
-
-
-
-
 
 def main(args):
     
     if args.llm_name == 'Mistral7B':
-        with open('/home/hippolytepilchen/code/embed_llm/config/default_mistral.yaml') as file:
+        with open('/home/hippolytepilchen/code/embed_llm/config/default/default_mistral.yaml') as file:
             config = yaml.safe_load(file)
     elif args.llm_name == 'Gemma7B':
-        with open('/home/hippolytepilchen/code/embed_llm/config/default_gemma.yaml') as file:
+        with open('/home/hippolytepilchen/code/embed_llm/config/default/default_gemma.yaml') as file:
             config = yaml.safe_load(file)
     elif args.llm_name == 'Llama3.2-3B':
-        with open('/home/hippolytepilchen/code/embed_llm/config/default_mistral.yaml') as file:
+        with open('/home/hippolytepilchen/code/embed_llm/config/default/default_mistral.yaml') as file:
             config = yaml.safe_load(file)
     else:
         raise ValueError(f'{args.llm_name} not supported yet !')
@@ -49,28 +44,29 @@ def main(args):
     
     config['exp_name'] = args.prefix + sha1(name.encode("utf8")).hexdigest()[:20]
     config['wandb']['run_name'] = args.prefix + sha1(name.encode("utf8")).hexdigest()[:20]
-
-    train(config)
-
+    
+    with open(f'/home/hippolytepilchen/code/embed_llm/config/experiments/{config["exp_name"]}.yaml', 'w') as file:
+        yaml.dump(config, file)
+    
 def arg_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--llm_name', type=str, default='Mistral7B', choice = ['Gemma7B', 'Llama3.2-3B', 'Mistral7B'])
+    parser.add_argument('--llm_name', type=str, default='Mistral7B', choices = ['Gemma7B', 'Llama3.2-3B', 'Mistral7B'])
     parser.add_argument('--w_embeds', action='store_true', help='Whether to use word embeddings as preconditioning')
     parser.add_argument('--norm_wo_embeds', action='store_true', help='Whether to normalize without word embeddings if using w_embeds')
     parser.add_argument('--proj_hidden_dim', type=int, default=4096, help='Hidden dimension of the projection MLP')
     parser.add_argument('--proj_n_layers', type=int, default=3, help='Number of layers of the projection MLP')
-    parser.add_argument('--proj_act', type=str, default='id', help='Activation function of the projection MLP', choice= ['id', 'gelu', 'relu'])
-    parser.add_argument('--batch_size', type=int, default=4, help='Batch size')
-    parser.add_argument('--max_lr', type=float, default=1e-4, help='Maximum learning rate')
-    parser.add_argument('--max_steps', type=int, default=100000, help='Maximum number of steps')
+    parser.add_argument('--proj_act', type=str, default='id', help='Activation function of the projection MLP', choices = ['id', 'gelu', 'relu'])
+    parser.add_argument('--batch_size', type=int, default=8, help='Batch size')
+    parser.add_argument('--max_lr', type=float, default=5e-6, help='Maximum learning rate')
+    parser.add_argument('--max_steps', type=int, default=10000, help='Maximum number of steps')
     parser.add_argument('--pct_start', type=float, default=0.2, help='Percentage of steps used for the warm-up')
-    parser.add_argument('--initial_lr', type=float, default=1e-6, help='Initial learning rate')
-    parser.add_argument('--final_lr', type=float, default=1e-4, help='Final learning rate')
+    parser.add_argument('--initial_lr', type=float, default=1e-20, help='Initial learning rate')
+    parser.add_argument('--final_lr', type=float, default=1e-10, help='Final learning rate')
     parser.add_argument('--log_freq', type=int, default=100, help='Logging frequency')
     parser.add_argument('--eval_freq', type=int, default=1000, help='Evaluation frequency')
-    parser.add_argument('--ckpt_freq', type=int, default=1000, help='Checkpoint frequency')
+    parser.add_argument('--ckpt_freq', type=int, default=500, help='Checkpoint frequency')
     parser.add_argument('--prefix', type=str, default='default', help='Prefix for the experiment')
-    parser.add_argument('--seq_len', type=int, default=1024, help='Sequence length')
+    parser.add_argument('--seq_len', type=int, default=512, help='Sequence length')
     return parser.parse_args()
     
 if __name__ == '__main__':
--- experiments.sh
+++ experiments.sh
@@ -12,42 +12,13 @@
 #SBATCH --job-name=incontext_llm
 #SBATCH --output=/lustre/scwpod02/client/kyutai-interns/hippop/experiments/slurm_output/slurm_output_%A_%a.log 
 
-# Command Selection
-case $SLURM_ARRAY_TASK_ID in
+# Define hyperparameter values to test
+learning_rates=(0.001 0.01 0.1)
+batch_sizes=(32 64 128)
 
-
-1)  
-
-    for id_ex in 1 2 3 4 5; do
-        micromamba run -n comp_retriev python \
-            test_incontext.py -n_ex 5 -model_name "Mistral7B_base" -name_exp "Mistral7B_base_n_ex_$id_ex"  -id_ex $id_ex -n_test 10 -id_start 1
-    done
-    ;;
-2)
-    for id_ex in 1 2 3 4 5; do
-        micromamba run -n comp_retriev python \
-            test_incontext.py -n_ex 5 -model_name "Qwen7B_base" -name_exp "Qwen7B_base_n_ex_$id_ex"  -id_ex $id_ex -n_test 10 -id_start 1
-    done
-
-    ;;
-3)
-
-    for id_ex in 1 2 3 4 5; do
-        micromamba run -n comp_retriev python \
-            test_incontext.py -n_ex 5 -model_name "Llama8B_base" -name_exp "Llama8B_base_n_ex_$id_ex"  -id_ex $id_ex -n_test 10 -id_start 1
-    done
-
-    ;;
-
-4)
-
-    for id_ex in 1 2 3 4 5; do
-        micromamba run -n comp_retriev python \
-            test_incontext.py -n_ex 5 -model_name "Gemma7B" -name_exp "Gemma7B_n_ex_$id_ex"  -id_ex $id_ex -n_test 10 -id_start 1
-    done
-
-    ;;
-*)
-echo "Could not a find command for job $SLURM_ARRAY_TASK_ID"
-;;
-esac
+# Train model for each combination of hyperparameters
+for lr in "${learning_rates[@]}"; do
+  for bs in "${batch_sizes[@]}"; do
+    python train.py --learning-rate $lr --batch-size $bs
+  done
+done
