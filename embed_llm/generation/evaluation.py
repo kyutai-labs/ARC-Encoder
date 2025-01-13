kl and load ckpt fixed

embed_llm/generation/evaluation.py
@@ -138,7 +138,7 @@ def evaluate_QA(
                 context.append(data["passages"][0])
 
         c = list(zip(questions, context, answers))
-        random.shuffle(c)
+        random.shuffle(c, random = lambda: 0.42)
         questions, context, answers = zip(*c)
         
         print("Evaluation dataset loaded for", benchmark)
@@ -571,43 +571,152 @@ if __name__ == "__main__":
         run_names = [args.run_name]
     else:
         run_names = [
-            "pretrain_llm_trained_rec_singpassage_054f63f8",
-            "pretrain_both_trained_cont_singpassage_17c38ada",
-            "pretrain_llm_trained_cont_singpassage_5daaa6bc",
-            "pretrain_llm_trained_rec_multipassage_054f63f8",
-            "pretrain_both_trained_02_singpassage_0f6f2a1a",
-            "pretrain_both_trained_rec_multipassage_0f6f2a1a",
-            "pretrain_both_trained_rec_singpassage_0f6f2a1a",
-            "pretrain_both_trained_1cont_0.2textcont_singpassage_17c38ada",
-            "pretrain_both_trained_1cont_0.5textcont_singpassage_17c38ada",
-            "pretrain_llm_trained_02_singpassage_054f63f8",
-            "pretrain_llm_trained_05_singpassage_054f63f8",
-            "nopref_pretrain_llm_trained_cont_singpassage_5daaa6bc",
-            "nopref_pretrain_no_trained_cont_singpassage_5daaa6bc",
-            'nopref_pretrain_no_trained_rec_singpassage_054f63f8',
-            'nopref_pretrain_no_trained_rec_multipassage_054f63f8',
-            'pretrain_both_trained_05_singpassage_0f6f2a1a',]
+            # Done
+            # "pretrain_llm_trained_rec_singpassage_054f63f8",
+            # "pretrain_both_trained_cont_singpassage_17c38ada",
+            # "pretrain_llm_trained_cont_singpassage_5daaa6bc",
+            # "pretrain_llm_trained_rec_multipassage_054f63f8",
+            # "pretrain_both_trained_02_singpassage_0f6f2a1a",
+            # "pretrain_both_trained_rec_multipassage_0f6f2a1a",
+            # "pretrain_both_trained_rec_singpassage_0f6f2a1a",
+            # "pretrain_both_trained_1cont_0.2textcont_singpassage_17c38ada",
+            # "pretrain_both_trained_1cont_0.5textcont_singpassage_17c38ada",
+            # "pretrain_llm_trained_02_singpassage_054f63f8",
+            # "pretrain_llm_trained_05_singpassage_054f63f8",
+            # "nopref_pretrain_llm_trained_cont_singpassage_5daaa6bc",
+            # "nopref_pretrain_no_trained_cont_singpassage_5daaa6bc",
+            # 'nopref_pretrain_no_trained_rec_singpassage_054f63f8',
+            # 'nopref_pretrain_no_trained_rec_multipassage_054f63f8',
+            # 'pretrain_both_trained_05_singpassage_0f6f2a1a',]
+            # 'nopref_pretrain_both_trained_02_singpassage_0f6f2a1a',
+            # 'nopref_pretrain_llm_trained_02_singpassage_054f63f8',
+            # 'nopref_pretrain_llm_trained_rec_multipassage_054f63f8',
+            # 'nopref_pretrain_pool_trained_cont_singpassage_5daaa6bc',
+            # 'nopref_pretrain_nollm_trained_cont_singpassage_5darr64',
+            # 'nopref_pretrain_llm_trained_07_singpassage_054f63f8',
+            # 'nopref_pretrain_pool_trained_rec_singpassage_054f63f8',
+            # 'nopref_pretrain_both_trained_cont_singpassage_17c38ada',
+            # 'nopref_pretrain_llm_trained_rec_singpassage_054f63f8',
+            # 'nopref_pretrain_both_trained_1cont_0.5textcont_singpassage_17c38ada',
+            
+            # Doable
+
+
+             'nopref_pretrain_both_trained_1cont_0.2textcont_singpassage_17c38ada',
+             'nopref_pretrain_both_trained_02_singpassage_0f6f2a1a' 
+            
+            
         
-        
-        # 'nopref_pretrain_both_trained_02_singpassage_0f6f2a1a',
-        # 'nopref_pretrain_llm_trained_02_singpassage_054f63f8',
-        # 'nopref_pretrain_llm_trained_rec_multipassage_054f63f8',
-        # 'nopref_pretrain_pool_trained_cont_singpassage_5daaa6bc',
-        # 'nopref_pretrain_pool_trained_rec_singpassage_054f63f8',
-        # 'nopref_pretrain_both_trained_1cont_0',
-        # 'nopref_pretrain_both_trained_1cont_0',
-        # 'nopref_pretrain_both_trained_cont_singpassage_17c38ada',
+
+        # still training
         # 'nopref_pretrain_both_trained_rec_multipassage_0f6f2a1a',
         # 'nopref_pretrain_both_trained_rec_singpassage_0f6f2a1a',
-        # 'nopref_pretrain_llm_trained_rec_singpassage_054f63f8',
-        # 'nopref_pretrain_both_trained_07_singpassage_0f6f2a1a',
-        # 'nopref_pretrain_llm_trained_07_singpassage_054f63f8',
-        # 'nopref_pretrain_both_trained_05_singpassage_0f6f2a1a',
-        # 'nopref_pretrain_llm_trained_05_singpassage_054f63f8',
-
+        # 'nopref_pretrain_both_trained_07_singpassage_0f6f2a1a', 
+        # 'nopref_pretrain_both_trained_rec_singpassage_2gate_0f6f2a1a',
+        # 'nopref_pretrain_both_trained_rec_singpassage_16gate_0f6f2a1a',
+        # 'nopref_pretrain_both_trained_rec_singpassage_4gate_0f6f2a1a',
+        # 'nopref_pretrain_both_trained_rec_singpassage_8gate_0f6f2a1a',
+        # 'nopref_pretrain_llm_trained_rec_singpassage_8gate_054f63f8',
+        # 'nopref_pretrain_nollm_trained_rec_singpassage_8gate_5darr64',
+        # 'nopref_pretrain_no_trained_rec_singpassage_8gate_054f63f8',
+        # 'nopref_pretrain_both_trained_highuseless_hybrid_singpassage_0f6f2a1a',
+        # 'nopref_pretrain_both_trained_lownoembed_hybrid_singpassage_0f6f2a1a',
+        # 'nopref_pretrain_both_trained_std_hybrid_singpassage_0f6f2a1a',
+        # 'nopref_pretrain_both_trained_std_hybrid_multipassage_0f6f2a1a',
+        # 'nopref_pretrain_pool_trained_rec_singpassage_8gate_054f63f8',
+        # 'nopref_pretrain_llm_trained_std_hybrid_multipassage_054f63f8',
+        # 'nopref_pretrain_llm_trained_std_hybrid_singpassage_054f63f8',
+        # 'nopref_pretrain_nollm_trained_std_hybrid_multipassage_5darr64',
+        # 'nopref_pretrain_nollm_trained_std_hybrid_singpassage_5darr64',
+        # 'nopref_pretrain_no_trained_std_hybrid_multipassage_054f63f8',
+        # 'nopref_pretrain_no_trained_std_hybrid_singpassage_054f63f8',
+        # 'nopref_pretrain_pool_trained_std_hybrid_singpassage_054f63f8',
+        ]
 
     for i, run_name in enumerate(run_names):
 
+        print("Standard Dump")
+        pipeline, ckpt = evaluate_reconstruction_model(
+            run_name,
+            output_file=output_file,
+            temperatures=[0, 0.5, 0.7],
+            max_seq_len=max_seq_len,
+            tmp_path=tmp_path,
+            eval_data_type="standard_dump",
+            n_passages=n_passages,
+        )  # 'atlas','standard_dump'
+        
+        print("Atlas")
+        pipeline, ckpt = evaluate_reconstruction_model(
+            run_name,
+            output_file=output_file,
+            temperatures=[0, 0.5, 0.7],
+            max_seq_len=max_seq_len,
+            tmp_path=tmp_path,
+            eval_data_type="atlas",
+            pipeline=pipeline,
+            ckpt=ckpt,
+            n_passages=n_passages,
+        )
+
+
+
+        pipeline, ckpt = evaluate_QA(
+            run_name,
+             ["NQ","TRIVIAQA"],
+            temps=[0, 0.5],
+            max_bs=4,
+            output_file=output_file,
+            n_samples=n_passages,
+            max_seq_len=max_seq_len,
+            tmp_path=tmp_path,
+            icl_examples=0,
+            pipeline=pipeline,
+            ckpt=ckpt,
+        )
+        
+        pipeline, ckpt = evaluate_QA(
+            run_name,
+            ["NQ","TRIVIAQA"],
+            temps=[0, 0.5],
+            max_bs=4,
+            output_file=output_file,
+            n_samples=n_passages,
+            max_seq_len=max_seq_len,
+            tmp_path=tmp_path,
+            icl_examples=2,
+            pipeline=pipeline,
+            ckpt=ckpt,
+        )
+        
+        pipeline, ckpt = evaluate_QA(
+            run_name,
+            ["NQ","TRIVIAQA"],
+            temps=[0, 0.5],
+            max_bs=4,
+            output_file=output_file,
+            n_samples=n_passages,
+            max_seq_len=max_seq_len,
+            tmp_path=tmp_path,
+            icl_examples=4,
+            pipeline=pipeline,
+            ckpt=ckpt,
+        )
+        torch.cuda.empty_cache()
+        print("Finished run", run_name)
+
+
+
+    # TODO IN 128 toks lim
+    run_names = [
+    # 'LT_FN_TrueMEAN_1_MLP_RLatt_True_CA_2_CAL_every_True_DB',
+    # 'LT_FN_TrueMEAN_1_MLP_Latt_True_CA_2_CAL_every_True_DB',
+    # 'LT_FN_Truemean_1_MLP_8_TRUNC_True_CA_2_CAL_every_True_DB',
+    'LT_FN_Truelatent_attention_3_MLP_8_TRUNC_True_CA_2_CAL_every_True_DB']
+
+   
+    max_seq_len = 128
+    for i, run_name in enumerate(run_names):
         print("Standard Dump")
         pipeline, ckpt = evaluate_reconstruction_model(
             run_name,
@@ -637,7 +746,7 @@ if __name__ == "__main__":
         pipeline, ckpt = evaluate_QA(
             run_name,
              ["NQ","TRIVIAQA"],
-            temps=[0, 0.5, 0.7],
+            temps=[0, 0.5],
             max_bs=4,
             output_file=output_file,
             n_samples=n_passages,
@@ -651,7 +760,7 @@ if __name__ == "__main__":
         pipeline, ckpt = evaluate_QA(
             run_name,
             ["NQ","TRIVIAQA"],
-            temps=[0, 0.5, 0.7],
+            temps=[0, 0.5],
             max_bs=4,
             output_file=output_file,
             n_samples=n_passages,
@@ -665,7 +774,7 @@ if __name__ == "__main__":
         pipeline, ckpt = evaluate_QA(
             run_name,
             ["NQ","TRIVIAQA"],
-            temps=[0, 0.5, 0.7],
+            temps=[0, 0.5],
             max_bs=4,
             output_file=output_file,
             n_samples=n_passages,
@@ -678,47 +787,3 @@ if __name__ == "__main__":
         torch.cuda.empty_cache()
         print("Finished run", run_name)
 
-
-
-    # # TODO IN 128 toks lim
-    # # 'LT_FN_TrueMEAN_1_MLP_RLatt_True_CA_2_CAL_every_True_DB',
-    # # 'LT_FN_TrueMEAN_1_MLP_Latt_True_CA_2_CAL_every_True_DB',
-    # # 'LT_FN_Truemean_1_MLP_8_TRUNC_True_CA_2_CAL_every_True_DB',
-    # # 'LT_FN_Truelatent_attention_3_MLP_8_TRUNC_True_CA_2_CAL_every_True_DB',
-
-    # run_names = ['LT_FN_Truemean_1_MLP_8_TRUNC_True_CA_2_CAL_every_True_DB',]#'LT_FN_TrueMEAN_1_MLP_RLatt_True_CA_2_CAL_every_True_DB','LT_FN_TrueMEAN_1_MLP_Latt_True_CA_2_CAL_every_True_DB']
-    # max_seq_len = 128
-    # for i, run_name in enumerate(run_names):
-    #     print("Standard Dump")
-    #     evaluate_reconstruction_model(
-    #         run_name,
-    #         output_file=output_file,
-    #         temperatures=[0, 0.5, 0.7, 1.0],
-    #         max_seq_len=max_seq_len,
-    #         tmp_path=tmp_path,
-    #         eval_data_type="standard_dump",
-    #     )  # 'atlas','standard_dump'
-    #     print("Atlas")
-    #     evaluate_reconstruction_model(
-    #         run_name,
-    #         output_file=output_file,
-    #         temperatures=[0, 0.5, 0.7, 1.0],
-    #         max_seq_len=max_seq_len,
-    #         tmp_path=tmp_path,
-    #         eval_data_type="atlas",
-    #     )
-
-
-    #     evaluate_QA(
-    #         run_name,
-    #         ["NQ"],#, "TRIVIAQA"],
-    #         temps=[0, 0.5, 0.7, 1.0],
-    #         max_bs=4,
-    #         output_file=output_file,
-    #         n_samples=100,
-    #         max_seq_len=max_seq_len,
-    #         tmp_path=tmp_path,
-    #         icl_examples=1,
-    #     )
-    #     torch.cuda.empty_cache()
-    #     print("Finished run", run_name)

embed_llm/models/wrapped_models_training.py
@@ -337,7 +337,7 @@ def load_training_model_from_ckpt(
         assert (
             not tune_embedder
         ), "If no trainable embedder, embedder model can't be instruct tuned"
-    if not old_pipeline_args.trainable_llm:
+    if not old_pipeline_args.trainable_llm or not tune_llm:
         assert not tune_llm, "If no trainable llm, llm model can't be instruct tuned"
         llm_args.lora = None
 
@@ -401,6 +401,8 @@ def load_training_model_from_ckpt(
 
     with torch.device("meta"):
         augmented_model = augmented_pipeline.get_model(llm=llm)
+    
+    ignored_state = []
 
     if (
         old_pipeline_args.trainable_embedder or old_pipeline_args.train_only_pooling
@@ -418,8 +420,8 @@ def load_training_model_from_ckpt(
             augmented_model.pooling_module.process.latents = torch.nn.Parameter([v for k, v in state_dict.items() if "latents" in k][0].cuda())
            
             ignored_state = [augmented_model.pooling_module.process.latents]
-        else:
-            ignored_state = []
+            del state_dict
+
 
     if old_pipeline_args.mlp_project.n_layers > 0:
         main_logger_info("Loading MLP projector")
@@ -433,6 +435,8 @@ def load_training_model_from_ckpt(
         if "attention" in augmented_model.mlp_project_args.type:
             augmented_model.mlp_project.latents = torch.nn.Parameter([v for k, v in state_dict.items() if "latents" in k][0].cuda())
             ignored_state.append(augmented_model.mlp_project.latents)
+            
+        del state_dict
 
     if get_rank() == 0:
         if old_pipeline_args.cross_att:
@@ -446,10 +450,16 @@ def load_training_model_from_ckpt(
             augmented_model.llm.load_state_dict(
                 cross_att_state_dicts, assign=True, strict=False
             )
-        print('AUGMENTED MODEL', augmented_model.llm.args)
 
-        if tune_llm and Path(lora_path).exists():
+
+        if Path(lora_path).exists():
             main_logger_info("Loading LLM LoRA state dict")
+            
+            if not tune_llm:
+                augmented_model.llm.args.lora = None
+            else:
+                augmented_model.llm.args.lora = lora
+                
             augmented_model.llm.load_lora(
                 Path(lora_path), cross_att=old_pipeline_args.cross_att
             )
@@ -459,6 +469,7 @@ def load_training_model_from_ckpt(
         for name, param in augmented_model.named_parameters():
             if param.is_meta:
                 print(name)
+                
         assert not any(
             p.is_meta for n, p in augmented_model.named_parameters() if "latents" not in n
         ), "All parameters should be initialized by now"
@@ -474,7 +485,6 @@ def load_training_model_from_ckpt(
             p.is_meta for n, p in augmented_model.named_parameters() if 'latents' not in n
         ), "All parameters should be on meta"
 
- 
 
 
     ignored_state = None if len(ignored_state) == 0 else ignored_state
@@ -482,6 +492,9 @@ def load_training_model_from_ckpt(
 
     # only finetune LoRA, MLP projector and pooling parameters and freeze before wrapping
     # If lora not enable weights have already been merged
+
+
+            
     for name, param in augmented_model.named_parameters():
         if tune_llm and "llm" in name and ((lora.enable and "lora" in name) or not lora.enable):
             param.requires_grad = True
@@ -506,15 +519,10 @@ def load_training_model_from_ckpt(
 
     
     main_logger_info(f"Sharding model over {get_world_size()} GPUs ...")
-    # Profiling memory
-    def get_gpu_memory():
-        command = "nvidia-smi"
-        memory_free_info = sp.check_output(command.split()).decode("ascii")
-        return memory_free_info
-    print('MEMORY BEFORE FSDP', get_gpu_memory())
 
     torch.distributed.barrier()
     
+
     wrapped_model = FullyShardedDataParallel(
         augmented_model,
         sharding_strategy=ShardingStrategy.FULL_SHARD,  # Gradients, activations, and parameters are sharded
@@ -531,5 +539,4 @@ def load_training_model_from_ckpt(
 
     main_logger_info("Model sharded!")
 
-    print('MEMORY AFTER FSDP', get_gpu_memory())
     return (augmented_pipeline, wrapped_model)

train.py
@@ -194,7 +194,7 @@ def _train(
             args.start_from_ckpt_path
             + "/"
             + args.llm_name.lower()
-            + "/trainable_embedder"
+            + "/pooling_module"
         ).exists()
     else:
         trainable_embedder = args.pipeline.trainable_embedder
@@ -203,6 +203,7 @@ def _train(
         args.pipeline.trainable_embedder
         or trainable_embedder
         or args.pipeline.train_only_pooling
+        
     ):
         embedding_model = None
     else:
@@ -218,7 +219,6 @@ def _train(
 
         embedding_model.eval()
     """ Load LLM and tokenizers """
-
     param_dtype = torch.float32 if args.mixed_precision else torch.bfloat16
     args.pipeline.param_dtype = param_dtype
 
@@ -329,7 +329,7 @@ def _train(
                     eval_batches_4cont.append(batch)
         else:
             eval_batches_4cont = None
-
+            
     # 9. Load optimizer
     optimizer = AdamW(
         model.parameters(),
@@ -424,8 +424,7 @@ def _train(
 
             # start_time = time.time()
             x, y, y_mask, seqlens, embeddings, embed_seqlens = prepare_batch_fn(batch)
-
-            if args.textual_continuation * args.continuation > 0.0 or args.hybrid_task.prop_noembed_continuation > 0.0:
+            if args.textual_continuation * args.continuation > 0.0 or (args.hybrid_task.prop_noembed_continuation > 0.0 and args.hybrid_task.do):
                 rand_noembed_continuation = (
                     torch.rand(1).cuda()
                     if get_rank() == 0
@@ -505,6 +504,7 @@ def _train(
                 
             # print('PREPARE BATCH TIME',"--- %s seconds ---" % (time.time() - start_time))
             # with profile(use_cuda = True) as prof:
+       
             output = model.forward(
                 x=x,
                 embeddings=embeddings,
@@ -531,7 +531,7 @@ def _train(
 
                 if args.instruct_tuning.cross_entropy:
                     mb_loss = instruct_cross_entropy
-
+                    
                 if args.instruct_tuning.kl:
                     contexts = [to_embed["tokens"] for to_embed in batch.to_embed]
                     x_rag = []
@@ -562,13 +562,17 @@ def _train(
                     assert len(x_rag) == len(
                         y_mask_rag
                     ), "x_rag and y_mask_rag should be the same length"
-                    rag_output = model.forward(
-                        x=x_rag,
-                        embeddings=embeddings,
-                        seqlens=seqlens_rag,
-                        embed_seqlens=embed_seqlens,
-                        batch_type=batch.data_type,
-                    )
+                    
+                    with torch.no_grad():
+                        model.eval()
+                        rag_output = model.forward(
+                            x=x_rag,
+                            embeddings=None,
+                            seqlens=seqlens_rag,
+                            embed_seqlens=embed_seqlens,
+                            batch_type=batch.data_type,
+                        )
+                        model.train()
 
                     kl_dv_loss = compute_kl_loss_with_mask(
                         rag_logits=rag_output,
@@ -581,6 +585,7 @@ def _train(
                     kl_loss += kl_dv_loss.item()
                     mb_loss = mb_loss + args.instruct_tuning.alpha * kl_dv_loss
 
+                    
                 mb_loss.backward()
                 loss += mb_loss.item()
                 # print(prof.key_averages().table(sort_by="cuda_time_total"))